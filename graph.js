e7439cc6f1c124cc96506037f59b0ce3fa8ab10d
jimkang <jimkang@gmail.com>
2013-07-03 08:53:06 -0400

    Moved svg declaration into html.

--- graph.js
+++ graph.js
@@ -116,7 +116,7 @@ function setUpGraph(stackData) {
       .orient('bottom')
       .tickValues(xAxisDateStrings);
    
-  var svg = d3.select('body').append('svg')
+  var svg = d3.select('svg')
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
     .append('g')
